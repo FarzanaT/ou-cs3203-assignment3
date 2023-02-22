e7d2e18e300c2dc2fed052122eb7476728ae9660
Farzana Tasnim <ftasnim074@gmail.com>
2023-02-21 20:44:27 -0600

    prodcut function added

--- Final_Soft_Engr/src/First_Class.java
+++ Final_Soft_Engr/src/First_Class.java
@@ -5,14 +5,22 @@ public class First_Class {
 		System.out.println(("working"));
 		System.out.println(("working"));
 		int[]arr = {1, 2, 3};
-		System.out.println(counting(arr));
+		System.out.println(countingProduct(arr));
 		
 }
 	
-	public static int counting(int[]arr) {
+	public static int countingSum(int[]arr) {
 		int sum = 0;
 		for(int x:arr) {
 			sum+= x;
+		}
+		return sum; //
+	}
+	
+	public static int countingProduct(int[]arr) {
+		int sum = 1;
+		for(int x:arr) {
+			sum = sum*x;
 		}
 		return sum; //
 	}
