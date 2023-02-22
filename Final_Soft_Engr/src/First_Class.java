afcfc6f3b36d2c4055a767139758aa2d8e4aeed9
Farzana Tasnim <ftasnim074@gmail.com>
2023-02-21 20:41:47 -0600

    counting function created

--- Final_Soft_Engr/src/First_Class.java
+++ Final_Soft_Engr/src/First_Class.java
@@ -1,8 +1,19 @@
+import java.util.*;
 
 public class First_Class {
 	public static void main(String []args){
 		System.out.println(("working"));
 		System.out.println(("working"));
+		int[]arr = {1, 2, 3};
+		System.out.println(counting(arr));
 		
 }
+	
+	public static int counting(int[]arr) {
+		int sum = 0;
+		for(int x:arr) {
+			sum+= x;
+		}
+		return sum; //
+	}
 }
