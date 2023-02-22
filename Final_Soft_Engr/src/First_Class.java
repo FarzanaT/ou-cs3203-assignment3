5320a28e82a3db68f8e2d97835857900366195ce
Farzana Tasnim <ftasnim074@gmail.com>
2023-02-21 21:04:04 -0600

    reversed function

--- Final_Soft_Engr/src/First_Class.java
+++ Final_Soft_Engr/src/First_Class.java
@@ -6,6 +6,10 @@ public class First_Class {
 		System.out.println(("working"));
 		int[]arr = {1, 2, 3};
 		System.out.println(countingProduct(arr));
+		int[]tr = reversedArr(arr);
+		for (int i = 0; i<arr.length;i++) {
+			System.out.println(tr[i]);
+		}
 		
 }
 	
@@ -23,5 +27,14 @@ public class First_Class {
 			sum = sum*x;
 		}
 		return sum; //
+	}
+	
+	public static int[] reversedArr (int[]arr) {
+		int [] toReturnArray = new int[arr.length];
+		for (int i = arr.length-1, j = 0; i>=0; i--, j++) {
+				toReturnArray[j] = arr[i];
+			
+		}
+		return toReturnArray;
 	}
 }
