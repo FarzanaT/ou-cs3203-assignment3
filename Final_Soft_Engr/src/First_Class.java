79d0ea4d08d53c1ee999b542e4ae456431296f07
Farzana Tasnim <ftasnim074@gmail.com>
2023-02-21 21:35:37 -0600

    added to the main method

--- Final_Soft_Engr/src/First_Class.java
+++ Final_Soft_Engr/src/First_Class.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class First_Class {
-	public static void main(String []args){ //main method created
+	public static void main(String []args){ //main method added
 		Scanner myObj = new Scanner(System.in);
 		System.out.println(("working"));
 		System.out.println(("working"));
